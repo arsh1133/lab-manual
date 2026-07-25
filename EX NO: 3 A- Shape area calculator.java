52e686d089d345af93e0fed28a60d1843b8398b6
arsh1133 <arshath1133@gmail.com>
2026-07-25 08:57:51 +0530

    Create EX NO: 3 A- Shape area calculator.java

---
+++ EX NO: 3 A- Shape area calculator.java
@@ -0,0 +1,39 @@
+interface Shape {
+    double calculateArea();
+}
+
+class Circle implements Shape {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle implements Shape {
+    double length, width;
+
+    Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double calculateArea() {
+        return length * width;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5.0);
+        Shape rectangle = new Rectangle(4.0, 6.0);
+
+        System.out.println("Circle Area: " + circle.calculateArea());
+        System.out.println("Rectangle Area: " + rectangle.calculateArea());
+    }
+}
+
